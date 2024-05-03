351202b6047f0b1becae8246e0781767eb0d9ae5
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-03 14:39:27 +0530

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, noOfTimes) {
+    let repeatedString = "";
+    if (noOfTimes < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < noOfTimes; i++) {
+        repeatedString = repeatedString.concat(string);
+    }
 
+    return repeatedString;
 };
 
+console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
