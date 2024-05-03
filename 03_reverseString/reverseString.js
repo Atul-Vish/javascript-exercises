1df5834e5e6789d21f76fa9cf780753340acafc9
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-03 15:26:04 +0530

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Inputs: Cat
+    // Expected Output: taC
 
+    // 1.Grab the letters from the left to right
+    // 2.Add them with a +
+    let length = string.length;
+    let reversedString = "";
+    for (let i = length; i >= 0; i--) {
+        reversedString += string.charAt(i - 1);
+    }
+
+    return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
