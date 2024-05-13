5487da664c2296f0e0ca10ca017aeb5e4f6bde64
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-13 16:49:24 +0530

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = array.reduce((sum, curr) => {
+    return sum + curr;
+  },0);
+
+  return sum;
 };
 
-const multiply = function() {
+console.log(sum([2,3,5,7]));
 
+const multiply = function(array) {
+  let result = array.reduce((total, curr) => {
+    return total * curr;
+  }, 1);
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = Math.pow(a, b);
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let result = 1;
+	for (let i = 1; i <= factorial; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
