615d405a9c71a88b9d14ae303520e35b06bf32de
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-14 12:03:57 +0530

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (+number === 1) {
+        return 1;
+    }
 
+    if (+number < 0) {
+        return "OOPS";
+    }
+
+    let current = 1;
+    let previous = 0; 
+    let sum = 0;
+    for (let i = 0; i < +number - 1; i++) {
+        sum = current + previous;
+        previous = current;
+        current = sum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
