6fb720a49d29bf92d66a079e08893dcc4607f752
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-14 09:37:58 +0530

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+function palindromes(string) {
+    // How to remove punctuations from a String
+    let punctuation = /[\.,?! ]/g;
+    let text = string.replace(punctuation, "").toLowerCase();
+    let newText = text.split("").reverse().join("").toLowerCase();
 
+    return text === newText;
 };
+
+
+
+console.log(palindromes('Racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
